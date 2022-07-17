53f35960e4ab3566f41208095bbe1ee207640635
zxt6154 <zxt61542990@yeah.net>
2022-07-17 18:43:08 +0800

    add

---
+++ data_handle/src/main/java/com/example/data_handle/confiigs/RedisConfig.java
@@ -0,0 +1,27 @@
+package com.example.data_handle.confiigs;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+@ConfigurationProperties(prefix = "spring.redis")
+@Getter
+@Setter
+@NoArgsConstructor
+public class RedisConfig {
+    private String host;
+
+    private int port;
+
+    private int timeout;//秒
+
+    private int database;
+    //private String password;
+
+    private HashMap<String, String> pool = new HashMap<>();
+}
